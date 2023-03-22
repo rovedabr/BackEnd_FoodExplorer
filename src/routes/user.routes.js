8eb4902498ecc8e1762d74ee71d4aa27e2b07fc7
rovedabr <rovedabr@gmail.com>
2023-03-22 11:21:56 -0300

    Rotas de usuário - iniciado

---
+++ src/routes/user.routes.js
@@ -0,0 +1,11 @@
+const { Router, request, response } = require("express");
+
+const userRoutes = Router();
+
+userRoutes.post("/", (request, response) => {
+  const { name, email, password } = request.body;
+
+  response.json({name, email, password});
+});
+
+module.exports = userRoutes;
---
+++ src/server.js
@@ -0,0 +1,12 @@
+const { request, response } = require("express");
+const express = require("express");
+const routes = require("./routes/index");
+
+
+const app = express();
+app.use(express.json())
+app.use(routes)
+
+const PORT =  3333;
+
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
