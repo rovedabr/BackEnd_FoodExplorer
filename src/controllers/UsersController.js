94034f07461584d09b9a0480f3f73bd2cdd96d33
rovedabr <rovedabr@gmail.com>
2023-03-22 18:43:29 -0300

    criada a função update user

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,3 +1,4 @@
+const { hash } = require("bcryptjs")
 const AppError = require("../utils/AppError");
 const sqliteConnecction = require("../database/sqlite")
 
@@ -22,6 +23,40 @@ class UsersController {
 
     response.status(201).json()
   }
+
+  async update(request, response) {
+    const { name, email } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnecction();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+    if (!user) {
+      throw new AppError("Usuário não encontrado!");
+    }
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está cadastrado")
+    }
+
+    user.name = name;
+    user.email = email;
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      updated_at = ?
+      WHERE id = ?`,
+      [user.name, user.email, new Date(), id]
+    );
+
+    return response.status(200).json();
+  }
+
+  
 
 
 }
