b6aaeb1b08f9447cc8ddab5e96f05f06b3fdee07
rovedabr <rovedabr@gmail.com>
2023-03-23 15:44:32 -0300

    criada a tabela Ingredients

---
+++ src/controllers/IngredientsController.js
@@ -0,0 +1,11 @@
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError")
+
+class IngredientsController {
+  async create(request, response) {
+    
+  }
+
+}
+
+module.exports = IngredientsController
---
+++ src/database/knex/migrations/20230323182754_ingredients.js
@@ -0,0 +1,10 @@
+
+exports.up = knex => knex.schema.createTable("ingredientes", table => {
+  table.increments("id")
+  table.text("name").notNullable()
+  table.integer("meals_id").references("id").inTable("meals").onDelete("CASCADE")
+}) 
+
+exports.down = knex => knex.schema.dropTable("ingredients")
+  
+
