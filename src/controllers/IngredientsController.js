eaf65cb444756079285935661f80448bb0ada1f5
rovedabr <rovedabr@gmail.com>
2023-03-23 16:11:02 -0300

    criada a rota e a função para a tabela Ingredients

--- src/controllers/IngredientsController.js
+++ src/controllers/IngredientsController.js
@@ -3,7 +3,16 @@ const AppError = require("../utils/AppError")
 
 class IngredientsController {
   async create(request, response) {
-    
+    const { id, name } = request.body;
+    const { meals_id } = request.params;
+
+    const ingredients = await knex("ingredients").where({id}).first()
+    const meals = await knex("meals").where({ meals_id: id }).orderBy("id")
+
+    return response.json({
+      ...ingredients,
+      meals
+    })
   }
 
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 
 const userRouter = require("./user.routes")
 const mealsRouter = require("./meals.routes")
+const ingredientsRouter = require("./ingredients.routes")
 
 const routes = Router()
 
 routes.use("/users", userRouter)
 routes.use("/meals", mealsRouter)
+routes.use("/ingredients", ingredientsRouter)
 
 module.exports = routes
