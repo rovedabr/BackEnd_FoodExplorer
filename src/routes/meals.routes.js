67003cfd5b5dae34803ec3a17ef8baae3699e959
rovedabr <rovedabr@gmail.com>
2023-03-28 15:28:40 -0300

    Criados os arquivos para upload de imagem

--- src/routes/meals.routes.js
+++ src/routes/meals.routes.js
@@ -1,8 +1,12 @@
 const { Router, request, response } = require("express");
+const multer = require("multer");
+const uploadsConfig = require("../configs/upload")
 
 const MealsController = require("../controllers/MealsController");
+const ensureAuthenticated = require("../middleware/ensureAuthenticated")
 
 const mealsRoutes = Router();
+const upload = multer(uploadsConfig.MULTER)
 
 const mealsController = new MealsController();
 
@@ -10,5 +14,9 @@ mealsRoutes.post("/", mealsController.create);
 mealsRoutes.get("/:id", mealsController.show);
 mealsRoutes.get("/", mealsController.index);
 mealsRoutes.delete("/:id", mealsController.delete)
+mealsRoutes.patch("/:id", ensureAuthenticated, upload.single("image"), (request, response) => {
+  console.log(request.file.filename)
+  response.json()
+} )
 
 module.exports = mealsRoutes;
