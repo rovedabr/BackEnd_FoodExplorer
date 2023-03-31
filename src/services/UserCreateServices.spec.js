dcbc0bbb5dec12947f7617bff9b2803dda6ba6f1
rovedabr <rovedabr@gmail.com>
2023-03-31 18:15:45 -0300

    Teste de criação de usuário emil

--- src/services/UserCreateServices.spec.js
+++ src/services/UserCreateServices.spec.js
@@ -1,5 +1,7 @@
 const UserCreateServices = require("./UserCreateServices");
 const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory");
+const UserRepository = require("../repositories/UserRepository");
+const AppError = require("../utils/AppError.js");
 
 describe("UserCreateServices", () => {
   it("user should be create", async () => {
@@ -16,6 +18,28 @@ describe("UserCreateServices", () => {
   
     console.log(userCreated)
     expect(userCreated).toHaveProperty("id");
+  });
+
+  it("user not should be create with exists email", async () => {
+    const userTest1 = {
+      name: "User Test 1",
+      email: "usertest@test.com",
+      password: "111",
+      admin: 0
+    }
+
+    const userTest2 = {
+      name: "User Test 2",
+      email: "usertest@test.com",
+      password: "222",
+      admin: 0
+    }
+
+    const userInRepository = new UserRepositoryInMemory();
+    const userCreateServices = new UserCreateServices(UserRepository);
+
+    await userCreateServices.execute(userTest1);
+    await expect(userCreateServices.execute(userTest2)).rejects.toEqual(new AppError("Este e-mail não está cadastrado"));
   })
 
 })
