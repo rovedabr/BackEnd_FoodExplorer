fcb1517ee40cda318d7917bd46fc2ad898df0958
rovedabr <rovedabr@gmail.com>
2023-04-28 18:46:27 -0300

    Refatoração controllers (separação do update)

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -17,49 +17,9 @@ class UsersController {
     const { name, email, password, old_password } = request.body;
     const user_id = request.user.id;  
 
-    // const database = await sqliteConnection();
-
     const userRepository = new UserRepository();
     const userUpdateServices =new UserUpdateServices(userRepository);
-    await userUpdateServices.execute({ name, email, password, old_password })
-
-    // const user = await database.get('SELECT * FROM users WHERE id = (?)', [user_id]);
-
-    // const userWithEmail = await knex("users").select("email").where({email})
-    // const userWithUpdatedEmail = userWithEmail.length
-
-    // if(userWithUpdatedEmail === 0 ) {
-    //   throw new AppError("Este e-mail não está cadastrado")
-    // }
-
-    // if (password && old_password) {
-    //   const checkOldPassword = await compare(old_password, user.password)
-
-    //   if (!checkOldPassword) {
-    //     throw new AppError("A senha antiga não confere.")
-    //   }
-
-    //   user.password = await hash(password, 10)
-    // }
-
-    // user.name = name ?? user.name;
-    // user.email = email ?? user.email;
-    // user.admin = admin ?? user.admin;
-
-    // if (password && !old_password) {
-    //   throw new AppError("Você precisa informar a senha antiga para redefinição da nova senha.")
-    // }
-
-    // await database.run(`
-    //   UPDATE users SET
-    //   name = ?,
-    //   email = ?,
-    //   password = ?,
-    //   admin = ?,
-    //   updated_at = DATETIME("now")
-    //   WHERE id = ?`,
-    //   [user.name, user.email, user.password, user.admin, user_id]
-    // );
+    await userUpdateServices.execute({ name, email, password, old_password });
 
     return response.status(200).json();
   }
--- src/services/UserUpdateServices.js
+++ src/services/UserUpdateServices.js
@@ -7,17 +7,8 @@ class UserUpdateServices {
   }
 
   async execute({ name, email, password, old_password , admin }) {
-    // const user_id = request.params.id
 
     const user = await this.userRepository.findByEmail({ email })
-
-    // const user = await this.userRepository.findUser({id: user_id})
-    // console.log(user)
-
-    // const user = await database.get('SELECT * FROM users WHERE id = (?)', [user_id]);
-    
-    // const userWithEmail = await knex("users").select("email").where({email})
-    // const userWithUpdatedEmail = userWithEmail.length
 
     if(!user ) {
       throw new AppError("Este e-mail não está cadastrado")
@@ -29,36 +20,21 @@ class UserUpdateServices {
 
     if (password && old_password) {
       const checkOldPassword = await compare(old_password, user.password)
-      // console.log(checkOldPassword)
 
       if (!checkOldPassword) {
         throw new AppError("A senha antiga não confere.")
       }
       
       user.password = await hash(password, 10)
-      // console.log(user.password)
     }
-    // const newPassword = await hash(password, 10)
-    // console.log(newPassword)
-
-    // hashPassword = user.password
 
     user.name = name ?? user.name;
     user.email = email ?? user.email;
     user.admin = admin ?? user.admin;
-    // user.password = hashPassword ?? user.password
-// console.log(name)
-  //  const updated_at = knex.fn.now()
-      // const newUpdated_at = await this.userRepository.updateTime({updated_at})
-      // console.log(newUpdated_at)
-  //  const hashPassword = await hash(password, 10)
-    // console.log(user.password)
 
-  const userUpdated =  await this.userRepository.update(user.name, user.email, user.password);
+    const userUpdated =  await this.userRepository.update(user.name, user.email, user.password);
 
-  return userUpdated;
-    // console.log((user.name, user.email, user.password))
-
+    return userUpdated;
   }
 }
 
