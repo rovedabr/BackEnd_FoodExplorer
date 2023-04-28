f226e4837b8601156e628fee5fe1cf7b9214266d
rovedabr <rovedabr@gmail.com>
2023-04-28 17:23:23 -0300

    Refatoração do código

--- src/routes/meals.routes.js
+++ src/routes/meals.routes.js
@@ -3,7 +3,7 @@ const uploadsConfig = require("../configs/upload");
 const multer = require("multer");
 
 const MealsController = require("../controllers/MealsController");
-const ImageController = require("../controllers/ImageController");
+// const ImageController = require("../controllers/ImageController");
 
 const ensureAuthenticated = require("../middleware/ensureAuthenticated");
 const ensureUserAdminVerify = require("../middleware/ensureUserAdminVerify");
@@ -12,13 +12,13 @@ const mealsRoutes = Router();
 const upload = multer(uploadsConfig.MULTER);
 
 const mealsController = new MealsController();
-const imageController = new ImageController();
+// const imageController = new ImageController();
 
 mealsRoutes.post("/",  ensureAuthenticated, ensureUserAdminVerify, upload.single("image"), mealsController.create);
 mealsRoutes.put("/:id",  ensureAuthenticated, ensureUserAdminVerify, upload.single("image"), mealsController.update);
 mealsRoutes.get("/:id", mealsController.show);
 mealsRoutes.get("/", mealsController.index);
 mealsRoutes.delete("/:id", ensureAuthenticated, ensureUserAdminVerify, mealsController.delete)
-mealsRoutes.patch("/:id", ensureAuthenticated, ensureUserAdminVerify, upload.single("image"), imageController.update);
+// mealsRoutes.patch("/:id", ensureAuthenticated, ensureUserAdminVerify, upload.single("image"), imageController.update);
 
 module.exports = mealsRoutes; 
